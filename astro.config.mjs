b550ab10633ceeeacf6ae50792a3389977922532
Diego Castillo <diego.castilloguajardo@gmail.com>
2023-08-15 19:51:40 -0400

    Add es and en language configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,5 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import astroI18next from "astro-i18next";
 
 // https://astro.build/config
 import vue from "@astrojs/vue";
@@ -8,5 +9,5 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [vue(), tailwind()]
+  integrations: [vue(), astroI18next(), tailwind()],
 });
